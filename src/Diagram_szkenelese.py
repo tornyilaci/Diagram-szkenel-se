40e42751771537ab04844ae8945af0bf1e14bafa
Tornyossy László <lala200104@gmail.com>
2022-11-23 10:00:32 +0100

    elso fajlok

---
+++ src/Diagram_szkenelese.py
@@ -0,0 +1,14 @@
+import cv2
+import os
+import numpy as np
+
+path = 'C:\Users\Laccer\OneDrive\Asztali gép\SZE\III. félév\Gepi latas\Diagram szkennelése\Diagram-szkenel-se\src\images\shape.jpg'
+print(os.path.exists(path))
+
+'''
+path = 'images\shape.jpg'
+img = cv2.imread(path)
+
+cv2.imshow("Original", img)
+cv2.waitKey(0)
+'''
